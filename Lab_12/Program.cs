7cc54bd8f87e17b52d2dc52b01b48f5e761fdf3c
elik <elik@example.com>
2020-12-05 23:22:50 +0300

    first commit

---
+++ Lab_12/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Lab_7.Controllers;
+using Lab_7.Exceptions;
+using Laba_7.Controllers;
+using System.Diagnostics;
+using Lab_12;
+using Laba_7.Models;
+using System.IO;
+
+namespace Laba_7
+{
+  
+    public interface IPerson
+    {
+        bool Person();
+
+    }
+  
+  
+    partial class Class1
+    { 
+    public void ICanRun()
+        { 
+            Console.WriteLine($"I Run");
+        }
+    }
+
+     
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Textbook myBook = new Textbook();
+            myBook.Age = 1215;
+            myBook.NameOfBook = "Alaverdi";
+            myBook.Price = 3.55;
+            myBook.Subject = "Mathematic";
+
+            Journal myJournal = new Journal();
+            myJournal.Age = 1815;
+            myJournal.Price = 50;
+            myJournal.Period = "Monthly";
+
+
+            //Magazine myMagazine = new Magazine();
+            //myMagazine.Age = 2015;
+            //myMagazine.Theme = "News";
+
+            Reflector.First("Laba_7.Journal");
+            Reflector.First("Laba_7.Journal");
+            Reflector.GetNameOfMethods("Laba_7.Journal", "System.String");
+            Reflector.GetNameOfMethods("Laba_7.Journal", "System.String");
+
+            string path = @"D:\OOP\Lab_12\param.txt";
+            string[] parms = { "", "" };
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    parms[0] = sr.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Random rnd = new Random();
+            parms[1] = Convert.ToString(rnd.Next(0, 20));
+            Reflector.Invoke(myBook, "ForLab", parms);
+
+            Textbook myBook2 = Reflector.Crate<Textbook>(typeof(Textbook));
+            myBook2.Age = 1823;
+            Console.WriteLine(myBook2.ToString());
+        }
+    }
+}
+
---
+++ Lab_12/Reflector.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Laba_7.Models;
+using System.Linq;
+using System.IO;
+
+namespace Lab_12
+{
+    public static class Reflector
+    {
+        public static void First(string t) 
+        {
+            string writePath = @"D:\OOP\Lab_12\File.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("");
+                }
+
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("Сборка");
+                    Console.WriteLine("Сборка");
+                    var assemdl = Type.GetType(t);
+                    sw.WriteLine(assemdl.Assembly.FullName);
+                    Console.WriteLine(assemdl.Assembly.FullName);
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                    sw.WriteLine("Имеются ли публичные конструкторы");
+                    Console.WriteLine("Имеются ли публичные конструкторы");
+                    ConstructorInfo[] p = assemdl.GetConstructors();
+                    for (int i = 0; i < p.Length; i++)
+                    {
+                        sw.WriteLine(p[i].IsPublic);
+                        Console.WriteLine(p[i].IsPublic);
+                    }
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                    sw.WriteLine("Методы");
+                    Console.WriteLine("Методы");
+                    var meth = assemdl.GetMethods();
+                    for (int i = 0; i < meth.Length; i++)
+                    {
+                        if (meth[i].IsPublic)
+                        {
+                            sw.WriteLine(meth[i]);
+                            Console.WriteLine(meth[i]);
+                        }
+                    }
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                    sw.WriteLine("Поля");
+                    Console.WriteLine("Поля");
+                    var fields = assemdl.GetFields();
+                    var propert = assemdl.GetProperties();
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        sw.WriteLine(fields[i]);
+                        Console.WriteLine(fields[i]);
+                    }
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                    sw.WriteLine("Свойства");
+                    Console.WriteLine("Свойства");
+                    for (int i = 0; i < propert.Length; i++)
+                    {
+                        sw.WriteLine(propert[i]);
+                        Console.WriteLine(propert[i]);
+                    }
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                    sw.WriteLine("Интерфейсы");
+                    Console.WriteLine("Интерфейсы");
+                    var interf = assemdl.GetInterfaces();
+                    for (int i = 0; i < interf.Length; i++)
+                    {
+                        sw.WriteLine(interf[i]);
+                        Console.WriteLine(interf[i]);
+                    }
+                    sw.WriteLine();
+                    Console.WriteLine();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void GetNameOfMethods(string ClassName, string TypeOfParams)
+        {
+            var type = Type.GetType(ClassName);
+            var param = Type.GetType(TypeOfParams);
+
+            var request = type.GetMethods().Where(i => i.GetParameters().Any(item => item.ParameterType == param));
+            if (request.Count() > 0)
+            {
+                Console.WriteLine($"All methods");
+                foreach (var item in request) Console.WriteLine(item);
+            }
+            else
+            {
+                Console.WriteLine("There no any methods");
+            }
+        }
+
+        public static void Invoke(object Class, string NameOfMethod, string[] Params)
+        {
+            // получаем метод GetArray
+            MethodInfo method = Class.GetType().GetMethod(NameOfMethod);
+            // вызываем метод, передаем значения для параметров и получаем
+            object result = method.Invoke(Class, Params);
+            Console.WriteLine((result));
+
+        }
+
+        public static T Crate<T>(Type myType) where T : class
+        {
+            ConstructorInfo info = myType.GetConstructors()[0];
+
+            // reflection (создаем объект, вызывая конструктор)
+            Object myObj = info.Invoke(null);
+
+            // result
+            return (T)myObj;
+        }
+    }
+}
